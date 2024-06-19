936da16caeded0e156c897a28bf20d1b6c4bb72d
taylorotwell <taylorotwell@users.noreply.github.com>
2024-06-19 13:30:13 +0000

    Fix code styling

--- src/Events/WorkerErrorOccurred.php
+++ src/Events/WorkerErrorOccurred.php
@@ -7,7 +7,5 @@ use Throwable;
 
 class WorkerErrorOccurred
 {
-    public function __construct(public Throwable $exception, public Application $sandbox)
-    {
-    }
+    public function __construct(public Throwable $exception, public Application $sandbox) {}
 }
