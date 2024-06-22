ace97c3aa00b515c54acb72cbad35cdec2849abd
taylorotwell <taylorotwell@users.noreply.github.com>
2024-06-22 22:29:03 +0000

    Fix code styling

--- tests/SequentialCoroutineDispatcherTest.php
+++ tests/SequentialCoroutineDispatcherTest.php
@@ -17,8 +17,7 @@ class SequentialCoroutineDispatcherTest extends TestCase
         ], $dispatcher->resolve([
             'first' => fn () => 1,
             'second' => fn () => 2,
-            'third' => function () {
-            },
+            'third' => function () {},
         ]));
     }
 }
